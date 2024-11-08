db354e29f1b562742b1f8e1968730d6215ac01d8
Nicholas Ly <hello@nicholasly.com>
2024-11-08 12:55:20 -0600

    refactor: phrasing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex items-center h-svh justify-center">
-      <h1 className="text-4xl font-semibold">Production</h1>
+      <h1 className="text-4xl font-semibold">Staging</h1>
     </main>
   );
 }
